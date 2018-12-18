5a724b8a85356fdc8b4bf4da6541964bba23a2a3
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-12-18 21:14:21 +0100

    Add tailwind
    - Also cleaned up old syntax
    - Added test-component

---
+++ src/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Message extends React.Component {
+ 	componentDidMount() {
+ 		console.log('Component Mounted');
+ 	}
+	render() {
+		return <h2 class="text-blue">{this.props.message ? this.props.message : 'Blankt Statement'}</h2>
+	}
+}
+
+export default Message;
